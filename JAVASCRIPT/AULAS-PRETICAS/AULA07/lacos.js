eef278b228e84cce9dec58436953495802b6a821
Paulo César <paulo_cesarcanal@hotmail.com>
2021-12-28 20:58:46 -0300

    AULA 07 - LAÇOS DE REPETIÇÃO
    Eu aprendi sobre laços de repetição, conheci o for, while e o do while.

---
+++ JAVASCRIPT/AULAS-PRETICAS/AULA07/lacos.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+
+    var repeticao
+
+    do{
+
+        repeticao++
+        console.log("Chegou ao fim o estoque de calças");
+
+    }while(repeticao < 10);
+
+    /*
+    for(var sapato = 0; sapato < 10; sapato++){
+
+        console.log("Chegou ao fim o estoque de sapatos!!");
+
+    }
+    
+    var repetição = 0;
+
+    while(repetição < 10) {
+        repetição++
+        console.log('Chegou ao fim seu estoque de calças!!');
+    }
+
+    */
+}
